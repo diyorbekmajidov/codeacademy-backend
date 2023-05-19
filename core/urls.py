5d885f0546a534d3d77610da8759187ef92c4517
Diyorbek <majidovdiyorbek741@gmail.com>
2023-05-19 15:48:07 +0500

    urls app Payments

--- core/urls.py
+++ core/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ClassRoom.urls')),
     path('lesson/', include('LessonPlanner.urls')),
+    path('payment/', include('PayMents.urls'))
 ]
