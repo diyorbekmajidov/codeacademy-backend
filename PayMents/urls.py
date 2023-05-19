f234e77ab3648c7b9c80413c09f3730f72e118e2
Diyorbek <majidovdiyorbek741@gmail.com>
2023-05-19 17:45:12 +0500

    paymentview

--- PayMents/urls.py
+++ PayMents/urls.py
@@ -1,6 +1,8 @@
-from .views import PaymentView
+from .views import PaymentView, Delet_Payment
 from django.urls import path
 
 urlpatterns = [
-    path('payment/', PaymentView.as_view())
+    path('payment/', PaymentView.as_view()),
+    path('payment/<int:pk>/', PaymentView.as_view()),
+    path('payment/delete/<int:pk>/', Delet_Payment.as_view()),
 ]
--- PayMents/views.py
+++ PayMents/views.py
@@ -6,14 +6,61 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
 from .models import Payment
+from datetime import datetime
+# import datetime
 
 
 # Create your views here.
 class PaymentView(APIView):
     def post(self, request):
-        serializer = PaymentSerializer(data=request.data)
+        data = request.data
+        d = datetime.strptime(data["start"], '%d/%m/%y')
+        e= datetime.strptime(data["end"], '%d/%m/%y')
+        myDT = datetime(d.year, d.month, d.day)
+        myDT2 = datetime(e.year, e.month, e.day)
+        mydata = myDT.strftime('%Y-%m-%dT%H:%M:%S.%f%z')
+        mydata2 = myDT2.strftime('%Y-%m-%dT%H:%M:%S.%f%z')
+        data["start"]= mydata
+        data["end"]= mydata2
+        serializer = PaymentSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors)
+    
+    def get(self, request):
+        payments = Payment.objects.all()
+        serializer = PaymentSerializer(payments, many=True)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        payment = Payment.objects.get(id=pk)
+        d = datetime.strptime(request.data["start"], '%d/%m/%y')
+        e= datetime.strptime(request.data["end"], '%d/%m/%y')
+        myDT = datetime(d.year, d.month, d.day)
+        myDT2 = datetime(e.year, e.month, e.day)
+        mydata = myDT.strftime('%Y-%m-%dT%H:%M:%S.%f%z')
+        mydata2 = myDT2.strftime('%Y-%m-%dT%H:%M:%S.%f%z')
+        request.data["start"]= mydata
+        request.data["end"]= mydata2
+        serializer = PaymentSerializer(payment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors)
+    
+class Delet_Payment(APIView):
+    def post(self, request, pk):
+        try:
+            payment = Payment.objects.get(id=pk)
+            payment.delete()
+            return Response("Payment Deleted")
+        except:
+            return Response("Payment Not Found")
+        
+        
+    
+
+        
 
