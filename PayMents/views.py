09b7a735e108b32fb795635391eb4877f734525c
Diyorbek <majidovdiyorbek741@gmail.com>
2023-05-19 15:47:40 +0500

    payment serializers

--- PayMents/views.py
+++ PayMents/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from .serializers import PaymentSerializer
+from rest_framework.views import APIView
+
+from rest_framework.response import Response
+from rest_framework.request import Request
+from .models import Payment
+
+
 # Create your views here.
+class PaymentView(APIView):
+    def post(self, request):
+        serializer = PaymentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
